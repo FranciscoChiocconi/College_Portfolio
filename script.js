055b4bfcee2ec6e4dd5f2be0f33ff61901384bae
Francisco Chiocconi <franchiocconi@MacBook-Pro.local>
2021-08-27 17:10:35 -0700

    navbar and projects

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector(".burger");
 const cover = document.querySelector(".cover");
+const sections = document.querySelectorAll("section");
+const navEl = document.getElementsByClassName(".main_nav");
+
 let menuOpen = false;
 
 menuBtn.addEventListener("click", function (e) {
@@ -14,4 +17,22 @@ menuBtn.addEventListener("click", function (e) {
     cover.classList.add("hidden");
     menuOpen = false;
   }
+});
+
+const options = {
+  threshold: 0.8,
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+  entries.forEach((entry) => {
+    console.log(navEl);
+    const className = entry.target.className.split("_")[0];
+    console.log(className);
+  });
+}
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
