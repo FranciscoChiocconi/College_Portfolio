1e2bbd9101211ed6fd6c3eec882bc43ac403f929
Francisco Chiocconi <franchiocconi@MacBook-Pro.local>
2021-09-01 16:45:43 -0700

    hamburger nav done, fixed about me on mobile

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
-const menuBtn = document.querySelector(".burger");
+const menuBtn = document.querySelector(".nav");
+const burgerbtn = document.querySelector(".menu-btn");
 const cover = document.querySelector(".cover");
 const sections = document.querySelectorAll("section");
 const navEl = document.getElementsByClassName(".main_nav");
-
+const element = document.querySelectorAll(".element");
 let menuOpen = false;
+let animationOpen = false;
 
-menuBtn.addEventListener("click", function (e) {
+const hide = function (e) {
   if (menuOpen === false) {
     cover.classList.remove("hidden");
     document.body.style.overflow = "hidden";
     document.body.style.height = "100%";
+    burgerbtn.classList.add("open");
     menuOpen = true;
   } else {
     document.body.style.overflow = "";
     document.body.style.height = "";
     cover.classList.add("hidden");
+    burgerbtn.classList.remove("open");
     menuOpen = false;
   }
-});
-
-const options = {
-  threshold: 0.8,
 };
+/*
+burgerbtn.addEventListener("click", () => {
+  if (animationOpen == false) {
+    burgerbtn.classList.add("open");
+    animationOpen = true;
+  } else {
+    burgerbtn.classList.remove("open");
+    animationOpen = false;
+  }
+});
+*/
+menuBtn.addEventListener("click", hide);
+element.forEach((el) => el.addEventListener("click", hide));
 
+/*
 let observer = new IntersectionObserver(navCheck, options);
 
 function navCheck(entries) {
   entries.forEach((entry) => {
-    console.log(navEl);
     const className = entry.target.className.split("_")[0];
-    console.log(className);
   });
 }
 
 sections.forEach((section) => {
   observer.observe(section);
 });
+*/
